49afd33e9e62552a5bea7af6796657a3481667c6
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-05-03 14:58:40 +0530

    Worked on Django views

--- myapps/tasks/views.py
+++ myapps/tasks/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from .models import Task
+from .forms import TaskForm
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 
 # Create your views here.
 class TasksView(View):
     def get(self, request):
+        tasks = list(Task.objects.values())
+        if request.is_ajax():
+            return JsonResponse({'tasks': tasks}, status=200)
         return render(request, 'tasks/tasks.html', {})
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            new_task = form.save()
+            return JsonResponse({'task': model_to_dict(form)}, status=200)
+
+        return redirect('tasks:tasks')
