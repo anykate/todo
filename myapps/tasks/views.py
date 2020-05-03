5a63089e20ffc042246ddf93d98a05a6517a32ea
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-05-03 12:08:42 +0530

    Renamed index page template from index.html to tasks.html

--- myapps/tasks/views.py
+++ myapps/tasks/views.py
@@ -5,4 +5,4 @@ from django.views.generic import View
 # Create your views here.
 class TasksView(View):
     def get(self, request):
-        return render(request, 'tasks/index.html', {})
+        return render(request, 'tasks/tasks.html', {})
